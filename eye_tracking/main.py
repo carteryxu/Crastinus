b45cf39128326e3aa905948d7fd5a4816224989f
carteryxu <carteryxu@gmail.com>
2024-09-21 00:14:48 -0400

    Removed blink detection

--- eye_tracking/main.py
+++ eye_tracking/main.py
@@ -15,25 +15,25 @@ predictor = dlib.shape_predictor(predictor_path)
 #Load camera
 cap = cv2.VideoCapture(0)
 
-def midpoint(p1, p2):
-    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+# def midpoint(p1, p2):
+#     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
-def get_blinking_ratio(eye_points, facial_landmarks):
-    left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
-    right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
-    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
-    center_bot = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+# def get_blinking_ratio(eye_points, facial_landmarks):
+#     left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+#     right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+#     center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+#     center_bot = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
 
-    # Drawing horizontal and vertical line
-    # cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
-    # cv2.line(frame, center_top, center_bot, (0, 255, 0), 2)
+#     # Drawing horizontal and vertical line
+#     # cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+#     # cv2.line(frame, center_top, center_bot, (0, 255, 0), 2)
 
-    # Calculating line ratios
-    vertical_length = hypot((center_top[0] - center_bot[0]), (center_top[1] - center_bot[1]))
-    horizontal_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+#     # Calculating line ratios
+#     vertical_length = hypot((center_top[0] - center_bot[0]), (center_top[1] - center_bot[1]))
+#     horizontal_length = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
     
-    ratio = horizontal_length / vertical_length
-    return ratio
+#     ratio = horizontal_length / vertical_length
+#     return ratio
 
 def get_gaze_ratio(eye_points, facial_landmarks):
     eye_region = np.array([(facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y),
@@ -89,12 +89,12 @@ while True:
         landmarks = predictor(gray, face)
 
         #Detect blinking
-        left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
-        right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
-        blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
+        # left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        # right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        # blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
 
-        if blinking_ratio > 4.45:
-            cv2.putText(frame, "BLINKING", (50, 150), font, 10, (0, 0, 255), 5)
+        # if blinking_ratio > 4.45:
+        #     cv2.putText(frame, "BLINKING", (50, 150), font, 10, (0, 0, 255), 5)
 
         #Gaze detection
         gaze_ratio_left_eye = get_gaze_ratio([36, 37, 38, 39, 40, 41], landmarks)
